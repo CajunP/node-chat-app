f175afcff3ba14626db23ef5af241424404e798a
Chulio <ivalasojarazo@gmail.com>
2018-04-17 17:55:55 -0300

    Add form for messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -13,4 +13,20 @@
 
   socket.on('newMessage', function(message) {
     console.log('New message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
   });
+
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault(); //para que no refreshee la pagina dsp del submit
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
+});
