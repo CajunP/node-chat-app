a5e75645a1e1a6885124af4b398903c692d80fa2
Chulio <ivalasojarazo@gmail.com>
2018-04-17 15:28:57 -0300

    Add newMessage event

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+
+  var socket = io();
+
+  socket.on('connect', function()  {
+    console.log('Connected to server');
+
+
+    socket.emit('createMessage', {
+      from:'marito@gmail.com',
+      text:'Marito agarramela con la manito'
+    });
+  });
+
+  socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+  });
+
+
+  socket.on('newMessage', function(message) {
+    console.log('New message', message);
+  });
