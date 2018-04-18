6f5abbd038aa000c7a57844d032aa319e7035eea
Chulio <ivalasojarazo@gmail.com>
2018-04-18 19:21:42 -0300

    Send messages to people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -85,7 +85,6 @@ jQuery('#message-form').on('submit', function (e) {
 
   //Down: Generates message inputted in the submit box
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('');
