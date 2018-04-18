ff1a2d8695065a4441b7f5d7551719ba99987f75
Chulio <ivalasojarazo@gmail.com>
2018-04-18 14:55:54 -0300

    Auto scroll added

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,25 @@
 
   var socket = io();
 
+//autoscroll
+  function scrollToBottom () {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+      messages.scrollTop(scrollHeight);
+    }
+  };
+
+
+
   socket.on('connect', function()  {
     console.log('Connected to server');
 
@@ -22,7 +41,7 @@
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
   });
 
 socket.on('newLocationMessage', function(message) {
@@ -37,7 +56,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
-
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
